0bf6ee399b9aecbc5fe136e9dc9a3eb2b78d82ea
Bhumika Mistry <mistrybhk@gmail.com>
2017-09-17 00:57:32 +0100

    basic moving away logic - simple

--- src/Block.java
+++ src/Block.java
@@ -17,82 +17,126 @@ public class Block extends Rectangle {
 
 	/**
 	 * Simply colours a block
-	 * @param g Graphics param passed from the frame
+	 * 
+	 * @param g
+	 *            Graphics param passed from the frame
 	 */
 	public void draw(Graphics g) {
 		g.setColor(colour);
 		g.fillRect(this.x, this.y, this.width, this.height);
 	}
-	
+
 	/**
-	 * Random movement - for now between -2 to 3
-	 * TODO - Add constrainsts so bounces off the frame edges
-	 * TODO - Change to Gaussian/ make more interesting
+	 * Random movement - for now between -2 to 3 TODO - Add constrainsts so
+	 * bounces off the frame edges TODO - Change to Gaussian/ make more
+	 * interesting
 	 */
-	public void move(){
-		int noiseX = random.nextInt(5)-2;
-		int noiseY = random.nextInt(5)-2;
-		
+	public void move() {
+		int noiseX = random.nextInt(5) - 2;
+		int noiseY = random.nextInt(5) - 2;
+
 		this.x += noiseX;
 		this.y += noiseY;
-		
+
 	}
-	
+
 	/**
-	 * Checks if the block is in overlapping any other blocks.
-	 * If so the colour will change for the block 
-	 * @param blocks List of all blocks 
+	 * Checks if the block is in overlapping any other blocks. If so the colour
+	 * will change for the block
+	 * 
+	 * @param blocks
+	 *            List of all blocks
 	 */
-	public void inContact(List<Block> blocks){
+	public void inContact(List<Block> blocks) {
 		boolean hasContact = false;
-		for(Block block : blocks){
-			if(this != block && this.intersects(block)){
+		for (Block block : blocks) {
+			if (this != block && this.intersects(block)) {
 				hasContact = true;
-				//Uncomment when not testing
-//				this.setColor(Color.BLUE);
-//				block.setColor(Color.BLUE);
-				
+				// Uncomment when not testing
+				// this.setColor(Color.BLUE);
+
+				// block.setColor(Color.BLUE);
+
 				Rectangle intersectionBlock = (Rectangle) this.intersection(block);
 				intersectionCoverage(intersectionBlock);
-//				System.out.println(intersectionBlock);
+				// System.out.println(intersectionBlock);
 			}
 		}
-		if(!hasContact){
-			//Uncomment when not testing
-//			this.setColor(Color.BLACK);
+		if (!hasContact) {
+			// Uncomment when not testing
+			// this.setColor(Color.BLACK);
 		}
 	}
-	
+
 	/**
 	 * Identifies which part of the block is covered the most
+	 * 
 	 * @param intersection
 	 */
-	public void intersectionCoverage(Rectangle intersection){
-		if(Math.abs(this.y-intersection.getY())< Math.abs(this.y+height - intersection.getY())){
-			System.out.println(intersection.toString()+ " TOP");
-		}else{
-			System.out.println(intersection.toString() +" BOTTOM");
+	public void intersectionCoverage(Rectangle intersection) {
+		String generalPos = "";
+		if (Math.abs(this.y - intersection.getY()) < Math.abs(this.y + height - intersection.getY())) {
+			System.out.println(this + " TOP");
+			generalPos += "T";
+		} else {
+			System.out.println(this + " BOTTOM");
+			generalPos += "B";
 		}
-		if(Math.abs(this.x-intersection.getX())< Math.abs(this.x+width - intersection.getX())){
-			System.out.println(intersection.toString()+ " LEFT");
-		}else{
-			System.out.println(intersection.toString() +" RIGHT");
+		if (Math.abs(this.x - intersection.getX()) < Math.abs(this.x + width - intersection.getX())) {
+			System.out.println(this + " LEFT");
+			generalPos += "L";
+		} else {
+			System.out.println(this + " RIGHT");
+			generalPos += "R";
 		}
+		moveAway(generalPos);
 	}
-	
-	public void setColor(Color colour){
+
+	public void moveAway(String interPos){
+		int xMove = 0;
+		int yMove = 0;
+		int noise = 30;
+		int ranPos = random.nextInt(noise)+1;
+		int ranNeg = random.nextInt(noise)-noise;
+		switch(interPos){
+			case("TL"):
+				xMove = ranPos;
+				yMove = ranPos;;
+				break;
+			case("TR"):
+				xMove = ranNeg;
+				yMove = ranPos;
+				break;
+			case("BL"):
+				xMove = ranPos;
+				yMove = ranNeg;
+				break;
+			case("BR"):
+				xMove = ranNeg;
+				yMove = ranNeg;
+				break;
+			default:
+				System.err.println("Invalid position description given");
+		}
+		
+		
+		this.x += xMove;
+		this.y += yMove;
+	}
+
+	public void setColor(Color colour) {
 		this.colour = colour;
 	}
-	
-	public Color getColor(){
+
+	public Color getColor() {
 		return this.colour;
 	}
-	
+
 	@Override
 	/**
 	 * Overriden to print the blocks dimensions
 	 */
-	public String toString(){
-		return "x: " + this.x+ " y: " + this.y + " height: "+this.height +" width: " + this.width;
+	public String toString() {
+		return "x: " + this.x + " y: " + this.y + " height: " + this.height + " width: " + this.width;
 	}
 }
